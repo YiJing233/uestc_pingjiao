77c260e1965ee06e9d326d0d1ee62cdf1908d423
YiKing <15567055305@163.com>
2020-12-28 13:20:17 +0800

    Create teacher.js

---
+++ teacher.js
@@ -0,0 +1,3 @@
+$("input[type=checkbox]").each(function() {    $(this).prop("checked", true);});
+document.querySelector("#evaText").innerText = "我对老师感到很满意。";
+document.querySelector("#main > form > table > tbody > tr:nth-child(20) > td > input[type=button]:nth-child(2)").click();
